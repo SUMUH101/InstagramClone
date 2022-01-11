6ab695afb9bfcf3629c9f99a62644dba992d3d67
Summer Warrican <summerwarrican7@hotmail.com>
2022-01-11 05:21:37 -0600

    Implemented Sign up and Sign Out functionality

--- app/src/main/java/com/swarrican/instagramclone/LoginActivity.java
+++ app/src/main/java/com/swarrican/instagramclone/LoginActivity.java
@@ -17,6 +17,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 import java.util.Objects;
 
@@ -25,6 +26,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etUserName;
     private EditText etPassword;
     private Button btnLogin;
+    private Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +47,7 @@ public class LoginActivity extends AppCompatActivity {
         etUserName = findViewById(R.id.etUserName);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        btnSignUp = findViewById(R.id.btnSignUp);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -54,11 +57,46 @@ public class LoginActivity extends AppCompatActivity {
                 loginUser(username,password);
             }
         });
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG,"onClick Sign Up Button");
+                String username = etUserName.getText().toString();
+                String password = etPassword.getText().toString();
+                SignUpUser(username,password);
+
+            }
+        });
 
     }
+
+    private void SignUpUser(String username, String password) {
+        Log.i(TAG,"Attempting to sign up user" + username);
+        // Create the ParseUser
+        ParseUser user = new ParseUser();
+// Set core properties
+        user.setUsername(username);
+        user.setPassword(password);
+// Invoke signUpInBackground
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    // Hooray! Let them use the app now.
+                    loginUser(username,password);
+                } else {
+                    // Sign up didn't succeed. Look at the ParseException
+                    // to figure out what went wrong
+                    Log.e(TAG, "Issue with sign up", e);
+                    return;
+                }
+            }
+        });
+
+    }
+
     private void loginUser(String username, String password){
         Log.i(TAG,"Attempting to login user" + username);
-        //TODO: navigate to the main activity if the user has in properly
+        //TODO: navigate to the main activity if the user has logged in properly
         ParseUser.logInInBackground(username, password, new LogInCallback() {
             @Override
             public void done(ParseUser user, ParseException e) {
--- app/src/main/java/com/swarrican/instagramclone/MainActivity.java
+++ app/src/main/java/com/swarrican/instagramclone/MainActivity.java
@@ -41,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnCaptureImage;
     private ImageView ivPostImage;
     private Button btnSubmit;
+    private Button btnSignOut;
     private File photoFile;
     public String photoFileName = "photo.jpg";
 
@@ -61,7 +62,8 @@ public class MainActivity extends AppCompatActivity {
         btnCaptureImage = findViewById(R.id.btnCaptureImage);
         ivPostImage = findViewById(R.id.ivPostImage);
         btnSubmit = findViewById(R.id.btnSubmit);
-        
+        btnSignOut = findViewById(R.id.btnSignOut);
+
         btnCaptureImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -86,6 +88,20 @@ public class MainActivity extends AppCompatActivity {
                 savePost(description,currentUser,photoFile);
             }
         });
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOut();
+                ParseUser currentUser = ParseUser.getCurrentUser();
+                goLoginActivity();
+            }
+        });
+    }
+
+    private void goLoginActivity() {
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+        finish();
     }
 
     private void launchCamera() {
